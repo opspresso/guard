ed12ae67788d26291ac82cacdff06a5092daf090
nalbam <me@nalbam.com>
2018-10-10 17:29:14 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Dockerfile
+
+FROM alpine
+
+RUN apk add --no-cache bash curl
+
+ENV VERSION 0
+RUN curl -sLO https://github.com/appscode/guard/releases/download/${VERSION}/guard-linux-amd64 && \
+    chmod +x guard-linux-amd64 && mv guard-linux-amd64 /usr/local/bin/guard
+
+ENTRYPOINT ["bash"]
---
+++ version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERNAME=${1}
+REPONAME=${2}
+GITHUB_TOKEN=${3}
+
+NOW=$(cat ./VERSION | xargs)
+NEW=$(curl -s https://api.github.com/repos/appscode/guard/releases/latest | grep tag_name | cut -d'"' -f4 | xargs)
+
+printf '# %-10s %-10s %-10s\n' "${REPONAME}" "${NOW}" "${NEW}"
+
+if [ "${NOW}" != "${NEW}" ]; then
+    printf "${NEW}" > VERSION
+    sed -i -e "s/ENV VERSION .*/ENV VERSION ${NEW}/g" Dockerfile
+
+    if [ ! -z ${GITHUB_TOKEN} ]; then
+        git config --global user.name "bot"
+        git config --global user.email "bot@nalbam.com"
+
+        git add --all
+        git commit -m "${NEW}"
+        git push -q https://${GITHUB_TOKEN}@github.com/${USERNAME}/${REPONAME}.git master
+
+        echo "# git push github.com/${USERNAME}/${REPONAME} ${NEW}"
+
+        git tag ${NEW}
+        git push -q https://${GITHUB_TOKEN}@github.com/${USERNAME}/${REPONAME}.git ${NEW}
+    fi
+fi
